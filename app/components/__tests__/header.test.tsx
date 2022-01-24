04241f56ab1ceb4720ccf2c1b74855b78c9b503c
Stephanie <stephaniiehallberg@gmail.com>
2022-01-24 08:47:49 +0100

    prettier, eslint, husky, and jest testing

--- app/components/__tests__/header.test.tsx
+++ app/components/__tests__/header.test.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import Header from '../header'
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from '../header';
 
 // !!!! Never write tests like this. It's basically useless
 // it's just here to make sure our testing setup works
 test('Renders Header', () => {
-  render(<Header />)
-  expect(
-    screen.getByRole('heading', {name: /Remix Tailwind Starter/}),
-  ).toBeInTheDocument()
-})
+    render(<Header />);
+    expect(
+        screen.getByRole('heading', { name: /Remix Tailwind Starter/ })
+    ).toBeInTheDocument();
+});
